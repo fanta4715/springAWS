3d78bbd679230b348e8e66f4886ec297f1eb3be3
fanta4715 <fanta4715@naver.com>
2023-07-12 20:07:41 +0900

    2023/07/12/PostsRepositoryTest까지

--- src/main/java/com/fanta4715/book/springboot/domain/post/PostsRepository.java
+++ src/main/java/com/fanta4715/book/springboot/domain/post/PostsRepository.java
@@ -1,7 +1,11 @@
 package com.fanta4715.book.springboot.domain.post;
 
 
-public interface PostsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
     //DB Layer접근자
-    //
+    //JpaRepository<Entity클래스, PK타입> 상속하면
+    //기본적인 CRUD 메소드 자동 생성
+    //Entity클래스와 함께 위치해야 함
 }
